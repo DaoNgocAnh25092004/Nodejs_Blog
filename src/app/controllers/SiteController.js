c211499c08a9711eed6f2aa27861f70392959681
DaoNgocAnh25092004 <Daongocanh25042004@gmail.com>
2024-10-16 19:51:39 +0700

    Bài 7: [CRUD] Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -2,19 +2,22 @@
 // Includes home page, search page, contact page, etc.
 
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     // [GET] /
-    async index(req, res) {
+    async index(req, res, next) {
         try {
             const courses = await Course.find({});
-            res.json(courses);
+            res.render('home', {
+                courses: multipleMongooseToObject(courses),
+            });
         } catch (err) {
-            res.status(400).json({ error: 'ERROR!!!' });
+            next(err);
         }
     }
 
     // [GET] /search
-    search(req, res) {
+    async search(req, res) {
         res.render('search');
     }
 }
